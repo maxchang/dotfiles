8f55512d85909093e2bfa2f500ee1b0b7a8f55ba
mchang <mchang@pilsner.salk.edu>
2016-02-05 10:01:18 -0800

    Added fish shell config files.

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+alias vi=vim
+set -x NFS_BIN /sanger/nfs_data/mchang/bin
+set -x PATH $HOME/bin $NFS_BIN /sanger/nfs_data/mchang/anaconda/bin $NFS_BIN/trinity $NFS_BIN/blast $NFS_BIN/tophat $NFS_BIN/bedtools $NFS_BIN/bedops /sanger/nfs_data/chris/genomics/homer/bin /sanger/nfs_data/chris/genomics/homer/scripts $PATH
+set -x BLASTDB /sanger/nfs_data/mchang/blast
