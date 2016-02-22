622ff0e6784e62ad41409eae1d25bbb6f5fe2019
Max Chang <maxwchang@gmail.com>
2016-02-22 13:58:16 -0800

    Add ngsplot path and variable.

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 alias vi=vim
 set -x NFS_BIN /sanger/nfs_data/mchang/bin
-set -x PATH $HOME/bin $NFS_BIN /sanger/nfs_data/mchang/anaconda/bin $NFS_BIN/trinity $NFS_BIN/blast $NFS_BIN/tophat $NFS_BIN/bedtools $NFS_BIN/bedops /sanger/nfs_data/chris/genomics/homer/bin /sanger/nfs_data/chris/genomics/homer/scripts $PATH
+set -x PATH $HOME/bin $NFS_BIN /sanger/nfs_data/mchang/anaconda/bin $NFS_BIN/trinity $NFS_BIN/blast $NFS_BIN/tophat $NFS_BIN/bedtools $NFS_BIN/bedops /sanger/nfs_data/chris/genomics/homer/bin /sanger/nfs_data/chris/genomics/homer/scripts $PATH /sanger/nfs_data/mchang/tools/ngsplot/bin
 set -x BLASTDB /sanger/nfs_data/mchang/blast
+set -x NGSPLOT /sanger/nfs_data/mchang/tools/ngsplot
