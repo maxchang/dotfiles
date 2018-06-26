42a2ed3832f9c65126d72a120b35c79e5c558a62
mwchang <mwchang@marvin.ucsd.edu>
2018-06-26 10:54:33 -0700

    Updated paths to use symbolic links to GPFS

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 alias vi=vim
-set -x PATH /home/mwchang/gpfs/bin /home/mwchang/gpfs/anaconda2/bin $PATH /gpfs/data01/cbenner/software/homer/bin /gpfs/data01/cbenner/software/homer/scripts /gpfs/data01/mchang/bin/ngsplot/bin
-set -x NGSPLOT /gpfs/data01/mchang/bin/ngsplot
+set -x PATH /home/mwchang/gpfs/bin /home/mwchang/gpfs/anaconda2/bin $PATH /home/mwchang/gpfs/bin/homer/bin /home/mwchang/gpfs/bin/ngsplot/bin
+set -x NGSPLOT /home/mwchang/gpfs/bin/ngsplot
 
 source (conda info --root)/etc/fish/conf.d/conda.fish
 set -gx CONDA_LEFT_PROMPT 1
